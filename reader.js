538d09f9dae3885dab76bc70ee47a9fb0de148ac
moms-spaghetti <williamedwards36@aol.com>
2021-05-25 17:00:55 +0100

    corrected time format milliseconds

--- reader.js
+++ reader.js
@@ -34,7 +34,7 @@ function outputFile(contents) {
 
   ednRows.map((item) => {
     const RFC_3339 = "YYYY-MM-DDTHH:mm:ss";
-    const timestamp = moment().format(RFC_3339 + "+00:00");
+    const timestamp = moment().format(RFC_3339 + "-00:00");
     const user = `${item.USER}${makeId(10)}`;
 
     ednDataStrings = [
